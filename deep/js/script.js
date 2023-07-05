e53380183e3c97eb22ae7b8d09cec52803ae81c1
Anstsha <97729752+Anstsha@users.noreply.github.com>
2023-07-05 15:13:06 +0300

    Add files via upload

---
+++ deep/js/script.js
@@ -0,0 +1,50 @@
+function ibg() {
+
+	let ibg = document.querySelectorAll(".ibg");
+	for (var i = 0; i < ibg.length; i++) {
+		if (ibg[i].querySelector('img')) {
+			ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
+		}
+	}
+}
+
+ibg();
+
+let options = {
+	root: null,
+	rootMargin: "0px 0px 0px 0px",
+	threshold: 0.15,
+};
+
+let callback = (entries) => {
+	entries.forEach((entry) => {
+		const targetElement = entry.target;
+		if (entry.isIntersecting && !targetElement.classList.contains('watched')) {
+			targetElement.classList.add('watched');
+		}
+	});
+}
+
+let observer = new IntersectionObserver(callback, options);
+let items = document.querySelectorAll("[data-watch]");
+items.forEach(item => {
+	observer.observe(item);
+
+});
+
+let isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+let isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+
+const classIbg = document.querySelector('.ibg');
+
+if (isIOS && isSafari) {
+	// iOS Safari: Do not add background-attachment: fixed
+	classIbg.classList.add('ios-safari');
+} else {
+	// Not iOS Safari: Add background-attachment: fixed
+	classIbg.classList.add('fixed-background');
+}
+
+document.querySelector('.footer__btn').onclick = () => {
+	window.scrollTo(pageYOffset, 0);
+}
